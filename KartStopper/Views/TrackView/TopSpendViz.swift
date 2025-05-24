9f5ab3c12fdb11c8ad58e46d0394d80315fe8df1
Ashish Brahma <ashish.brahma@outlook.com>
2025-05-24 18:40:21 +0530

    Refactor Top Spend pie chart and create custom disclosure for Today stat card.

---
+++ KartStopper/Views/TrackView/TopSpendViz.swift
@@ -0,0 +1,18 @@
+//
+//  TopSpendViz.swift
+//  KartStopper
+//
+//  Created by Ashish Brahma on 24/05/25.
+//
+
+import SwiftUI
+
+struct TopSpendViz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopSpendViz()
+}
